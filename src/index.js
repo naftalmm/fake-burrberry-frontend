f907f714a0fdfe0e0479b7692e1a9946394ac3f0
naftalmm <naftalmm@gmail.com>
2017-08-10 17:26:56 +0300

    feat: revert import fonts via webfontloader

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './../node_modules/flexboxgrid2/dist/flexboxgrid.min.css';
 import 'normalize.css';
-import WebFont from 'webfontloader';
-
-WebFont.load({
-  google: {
-    families: ['Lora:400,400i', 'Raleway:400,500,600,700']
-  }
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

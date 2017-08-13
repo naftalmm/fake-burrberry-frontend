14605e3392e64bf89a554c1635a43dbac33a868d
naftalmm <naftalmm@gmail.com>
2017-08-13 11:05:19 +0300

    fix: extract brakepoints constants in Responsive components

--- src/common/Responsive.jsx
+++ src/common/Responsive.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import Responsive from "react-responsive";
 
+const brakepoints = {
+  md: 768,
+  lg: 992
+};
+
 export const XS = ({ children }) =>
-  <Responsive maxWidth={767}>
+  <Responsive maxWidth={brakepoints.md - 1}>
     {children}
   </Responsive>;
 
 export const XS_MD = ({ children }) =>
-  <Responsive maxWidth={991}>
+  <Responsive maxWidth={brakepoints.lg - 1}>
     {children}
   </Responsive>;
 
 export const MD = ({ children }) =>
-  <Responsive minWidth={768} maxWidth={991}>
+  <Responsive minWidth={brakepoints.md} maxWidth={brakepoints.lg - 1}>
     {children}
   </Responsive>;
 
 export const MD_LG = ({ children }) =>
-  <Responsive minWidth={768}>
+  <Responsive minWidth={brakepoints.md}>
     {children}
   </Responsive>;
 
 export const LG = ({ children }) =>
-  <Responsive minWidth={992}>
+  <Responsive minWidth={brakepoints.lg}>
     {children}
   </Responsive>;
